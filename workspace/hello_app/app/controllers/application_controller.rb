0d6a3911b95ff5249938a828509ce7fa243b0ccb
Son Nguyen <ngocson2vn@gmail.com>
2017-01-19 08:38:33 +0000

    Add metric web.page_views

--- workspace/hello_app/app/controllers/application_controller.rb
+++ workspace/hello_app/app/controllers/application_controller.rb
@@ -1,3 +1,14 @@
+require 'datadog/statsd'
+
+# Create a statsd client instance.
+
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  @@statsd = Datadog::Statsd.new
+
+  def hello
+    @@statsd.increment('hello_app.web.page_views')
+    render html: "hello, world!"
+  end
+
 end
